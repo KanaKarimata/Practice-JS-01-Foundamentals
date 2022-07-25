dc3b50539879fd38c688766e51c749b185f5b4f6
KanaKarimata <kana.lt22@gmail.com>
2022-07-25 18:00:35 +0900

    Finish Part1-13

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-console.log ('Jonas');
-console.log(23);
+// console.log ('Jonas');
+// console.log(23);
 
-let firstName = "Jonas";
-// "firstName" is Valuables
-// "Jonas" is Values
+// let firstName = "Jonas";
+// // "firstName" is Valuables
+// // "Jonas" is Values
 
-let PI = 3.1415;
-let myFirstJob = 'programmer';
-let myCurrentJob = 'Teacher';
-// 👆likes
+// let PI = 3.1415;
+// let myFirstJob = 'programmer';
+// let myCurrentJob = 'Teacher';
+// // 👆likes
 
-let job1 = 'programmer';
-let job2 = 'Teacher';
-// 👆Not like
+// let job1 = 'programmer';
+// let job2 = 'Teacher';
+// // 👆Not like
 
-// assignment
-// 1
-let country = "Japan"
-let continent = "Asia"
-let population = "100 million"
+// // assignment
+// // 1
+// let country = "Japan"
+// let continent = "Asia"
+// let population = "100 million"
 
-// 2
-console.log(country)
-console.log(continent)
-console.log(population)
+// // 2
+// console.log(country)
+// console.log(continent)
+// console.log(population)
 
-true;
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
-// check Data type by log
-console.log(typeof true);
-console.log(typeof 23);
-console.log(typeof 'Jonas');
+// true;
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// // check Data type by log
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
 
+// year = 1991;
 
+let age = 30;
+age = 31;
+
+// const can't be changed
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+// var is old
+var job = 'programmer';
+job = 'teacher';
+
+// NG (without Valuables)
+lastName = 'Karimata';
+console.log(lastName);
