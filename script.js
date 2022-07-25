e57bc0ec884479790c4f6a2c7fce69f1974809b7
KanaKarimata <kana.lt22@gmail.com>
2022-07-25 17:25:08 +0900

    Finish Part1-10

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log ('Jonas');
+console.log(23);
+
+let firstName = "Jonas";
+// "firstName" is Valuables
+// "Jonas" is Values
+
+let PI = 3.1415;
+let myFirstJob = 'programmer';
+let myCurrentJob = 'Teacher';
+// 👆likes
+
+let job1 = 'programmer';
+let job2 = 'Teacher';
+// 👆Not like
+
