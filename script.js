dff4e8481a19f3faf1cce7bf79860e6e77b0a9f3
KanaKarimata <kana.lt22@gmail.com>
2022-07-26 19:43:24 +0900

    finish part1-16

--- script.js
+++ script.js
@@ -38,18 +38,53 @@
 
 // year = 1991;
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-// const can't be changed
-const birthYear = 1991;
-// birthYear = 1990;
-// const job;
+// // const can't be changed
+// const birthYear = 1991;
+// // birthYear = 1990;
+// // const job;
 
-// var is old
-var job = 'programmer';
-job = 'teacher';
+// // var is old
+// var job = 'programmer';
+// job = 'teacher';
 
-// NG (without Valuables)
-lastName = 'Karimata';
-console.log(lastName);
+// // NG (without Valuables)
+// lastName = 'Karimata';
+// console.log(lastName);
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(now - 1991 > now - 2018);
+
+// test data1
+let markWeight = 78
+let markHeight = 1.69
+let johnWeight = 92
+let johnHeight = 1.95
+
+const markBmi = markWeight / (markHeight * markHeight)
+const johnBmi = johnWeight / (johnHeight * johnHeight)
+
+console.log(markBmi)
+console.log(johnBmi)
+let markHigherBMI = markBmi > johnBmi
+console.log(markHigherBMI)
+console.log(typeof markHigherBMI)
+
+// test data2
+let markWeight2 = 95
+let markHeight2 = 1.88
+let johnWeight2 = 85
+let johnHeight2 = 1.76
+
+const markBmi2 = markWeight2 / (markHeight2 * markHeight2)
+const johnBmi2 = johnWeight2 / (johnHeight2 * johnHeight2)
+
+console.log(markBmi2)
+console.log(johnBmi2)
+let markHigherBMI2 = markBmi2 > johnBmi2
+console.log(markHigherBMI2)
+console.log(typeof markHigherBMI2)
