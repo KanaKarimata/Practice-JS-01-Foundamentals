694d668358b314fabd4b773d09ae0f7bb5c49f58
KanaKarimata <kana.lt22@gmail.com>
2022-07-28 20:53:33 +0900

    finish Part1-26

--- script.js
+++ script.js
@@ -236,18 +236,59 @@
 //   console.log('They are DRAW!');
 // }
 
-// Bonus1
-let dolphinsScore = (97 + 112 + 81) / 3;
-let koalasScore = (109+ 95 + 86) / 3;
-console.log(dolphinsScore);
-console.log(koalasScore);
+// Bonus1, Bonus2
+// let dolphinsScore = (97 + 112 + 81) / 3;
+// let koalasScore = (109+ 95 + 86) / 3;
+// console.log(dolphinsScore);
+// console.log(koalasScore);
 
-if (dolphinsScore > koalasScore && dolphinsScore >= 100){
-  console.log(`Winner is Dolphin!`);
-}else if (koalasScore > dolphinsScore && koalasScore >= 100) {
-  console.log(`Winner is Koalas!`);
-}else if (dolphinsScore === koalasScore && dolphinsScore >= 100 && koalasScore >=100) {
-  console.log(`Both min the trophy!`);
-}else {
-  console.log(`No one wins the trophy...`)
+// if (dolphinsScore > koalasScore && dolphinsScore >= 100){
+//   console.log(`Winner is Dolphin!`);
+// }else if (koalasScore > dolphinsScore && koalasScore >= 100) {
+//   console.log(`Winner is Koalas!`);
+// }else if (dolphinsScore === koalasScore && dolphinsScore >= 100 && koalasScore >=100) {
+//   console.log(`Both min the trophy!`);
+// }else {
+//   console.log(`No one wins the trophy...`)
+// }
+
+// using switch makes your code more readable
+const day = `wednesday`;
+
+switch(day) {
+  case `monday`://day === `monday`
+    console.log(`Plan course structure`);
+    console.log(`Go to coding meetup`);
+    break;
+  case `tuesday`:
+    console.log(`Prepare theory videos`);
+    break;
+  case `wednesday`:
+  case `thursday`:
+    console.log(`Write code examples`);
+    break;
+  case `friday`:
+    console.log(`Record videos`);
+    break;
+  case `saturday`:
+  case `sunday`:
+    console.log(`Enjoy the weekend :D`);
+    break;
+  default:
+    console.log(`Not a valid day!`);
+}
+
+if (day === `monday`) {
+  console.log(`Plan course structure`);
+    console.log(`Go to coding meetup`);
+} else if (day === `tuesday`) {
+  console.log(`Prepare theory videos`);
+} else if (day === `wednesday` || day === `thursday`) {
+  console.log(`Write code examples`);
+} else if (day === `friday`) {
+  console.log(`Record videos`);
+} else if (day === `saturday` || day === `sunday`) {
+  console.log(`Enjoy the weekend :D`);
+} else {
+  console.log(`Not a valid day!`);
 }
